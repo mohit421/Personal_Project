2a4dbb859e538c15c6f08fabef2e8f49224de563
Mohit <mohitjais312@gmail.com>
2024-10-23 00:07:34 +0530

    Learn how to use multiple layout and abou router setup Link tag and much more

--- React/TraversyMedia/react-jobs/src/App.jsx
+++ React/TraversyMedia/react-jobs/src/App.jsx
@@ -1,28 +1,44 @@
 // ````````````````````````` Start Home Page '''''''''''''''''''''''''''''''
 
+import { 
+  Route, 
+  createBrowserRouter, 
+  createRoutesFromElements,
+  RouterProvider,
+} from 'react-router-dom';
+import React from 'react';
 
-import React from 'react'
-import Navbar from '../src/components/Navbar';
-import Hero from './components/Hero';
-import HomeCards from './components/HomeCards';
-import JobListings from './components/JobListings';
-import ViewAllJobs from './components/viewAllJobs';
+import HomePage from './pages/HomePage'
+import MainLayout from './layouts/MainLayout';
+import JobPage from './pages/JobPage';
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    // <Route index element={<h1>Welcome to My App</h1>} />)
+    <Route path='/' element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path='/jobs' element={<JobPage />} />
+    </Route>
+  )
+    // <Route path='/about' element={<h1>Welcome to My App</h1>} />)
+    // (
+    // <>
+    // <Navbar />
+    // {/* We can use like just below line too  */}
+    // {/* <Hero title='Test Title' subtitle='This is the subtitle' /> */}
+    //    { /* if we use default then don't need to mention here  */}
+    // <Hero />
+    // <HomeCards />
+    // <JobListings />
+    // <ViewAllJobs />
+    // </>
+  // )
+);
 
 const App = () => {
-  return (
-    <>
-      <Navbar />
-      {/* We can use like just below line too  */}
-      {/* <Hero title='Test Title' subtitle='This is the subtitle' /> */}
-         { /* if we use default then don't need to mention here  */}
-      <Hero />
-      <HomeCards />
-      <JobListings />
-      <ViewAllJobs />
-    </>
-  )
-}
+  return <RouterProvider router={router} />;
+  
+};
 
 export default App
 
--- React/TraversyMedia/react-jobs/src/components/Navbar.jsx
+++ React/TraversyMedia/react-jobs/src/components/Navbar.jsx
@@ -1,41 +1,46 @@
 import React from 'react'
-import logo from '../assets/images/logo.png'
+import logo from '../assets/images/logo.png';
+import { Link } from 'react-router-dom';
+
+
+
 const Navbar = () => {
   return (
-    <nav class="bg-indigo-700 border-b border-indigo-500">
-      <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div class="flex h-20 items-center justify-between">
+    <nav className="bg-indigo-700 border-b border-indigo-500">
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="flex h-20 items-center justify-between">
           <div
             class="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
           >
             {/* <!-- Logo --> */}
-            <a class="flex flex-shrink-0 items-center mr-4" href="/index.html">
+            <Link className="flex flex-shrink-0 items-center mr-4" 
+            to="/">
               <img
-                class="h-10 w-auto"
+                className="h-10 w-auto"
                 src={ logo }
                 alt="React Jobs"
               />
-              <span class="hidden md:block text-white text-2xl font-bold ml-2"
+              <span className="hidden md:block text-white text-2xl font-bold ml-2"
                 >React Jobs</span
               >
-            </a>
-            <div class="md:ml-auto">
-              <div class="flex space-x-2">
-                <a
-                  href="/index.html"
-                  class="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Home</a
-                >
-                <a
-                  href="/jobs.html"
-                  class="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Jobs</a
-                >
-                <a
-                  href="/add-job.html"
-                  class="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Add Job</a
-                >
+            </Link>
+            <div className="md:ml-auto">
+              <div className="flex space-x-2">
+                <Link
+                  href="/"
+                  className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                  >Home
+                  </Link>
+                <Link
+                  to="/jobs"
+                  className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                  >Jobs
+                  </Link>
+                <Link
+                  to="/add-job"
+                  className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                  >Add Job
+                  </Link>
               </div>
             </div>
           </div>
