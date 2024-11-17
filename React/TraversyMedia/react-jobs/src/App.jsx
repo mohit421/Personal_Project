bd6bdaf5003f6ff84c7ef9b8cd318f2be9660eb4
Mohit <mohitjais312@gmail.com>
2024-11-17 15:05:38 +0530

    'Added all crud functionality in this'

--- React/TraversyMedia/react-jobs/src/App.jsx
+++ React/TraversyMedia/react-jobs/src/App.jsx
@@ -12,7 +12,44 @@ import JobsPage from './pages/JobsPage';
 import JobPage, {jobLoader } from './pages/JobPage';
 import NotFoundPage from './pages/NotFoundPage';
 import AddJobPage from './pages/AddJobPage';
+import EditJobPage from './pages/EditJobPage';
 
+
+const App = () => {
+  // Add new job 
+  const addJob =async (newJob) => {
+    // console.log(newJob);
+    const res = await fetch('/api/jobs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newJob)
+    });
+    return;
+}
+// Delete Job
+const deleteJob = async (id) => {
+  // console.log('delete', id);
+    const res = await fetch(`/api/jobs/${id}`, {
+      method: 'DELETE',
+    });
+    return;
+}
+
+// Update Job
+
+const updateJob = async (job) => {
+  // console.log(newJob);
+  const res = await fetch(`/api/jobs/${job.id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(job)
+  });
+  return;
+}
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -20,8 +57,12 @@ const router = createBrowserRouter(
     <Route path='/' element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path='/jobs' element={<JobsPage />} />
-      <Route path='/add-job' element={<AddJobPage />} />
-      <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
+      <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob} />} />
+      <Route path='/jobs/:id' element={<JobPage deleteJob = {deleteJob}/>} loader={jobLoader} />
+      <Route path='/edit-job/:id' 
+      element={<EditJobPage updatedJobSubmit = {updateJob} />} 
+      loader={jobLoader} />
+      
       {/* Any page that not found we just go there */}
       <Route path='*' element={<NotFoundPage />} />
     </Route>
@@ -42,10 +83,9 @@ const router = createBrowserRouter(
   // )
 );
 
-const App = () => {
   return <RouterProvider router={router} />;
   
 };
 
-export default App
+export default App;
 
