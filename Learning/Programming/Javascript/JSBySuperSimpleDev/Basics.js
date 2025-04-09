005c507634ebfb3e2ee753fd473571bfe073a567
Mohit <mohitjais312@gmail.com>
2025-04-09 13:50:18 +0530

    Revise S again

---
+++ Learning/Programming/Javascript/JSBySuperSimpleDev/Basics.js
@@ -0,0 +1,14 @@
+document.body.inner
+undefined
+document.body.innerHTML = 'Good Morning!'
+'Good Morning!'
+
+
+document.body.innerHTML = 'Mohit'
+'Mohit'
+10+8+20
+38
+100-20-50+200
+230
+document.body.innerHTML = ''
+''
