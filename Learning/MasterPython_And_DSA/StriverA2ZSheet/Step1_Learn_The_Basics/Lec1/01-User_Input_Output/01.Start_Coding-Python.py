ecf5be226824a9c2327e5ae58c484d4a6d7ec81e
Mohit <mohitjais312@gmail.com>
2025-04-27 14:36:24 +0530

    Learning JS

---
+++ Learning/MasterPython_And_DSA/StriverA2ZSheet/Step1_Learn_The_Basics/Lec1/01-User_Input_Output/01.Start_Coding-Python.py
@@ -0,0 +1,3 @@
+# Link:- https://www.geeksforgeeks.org/problems/start-coding-python--093158/1?selectedLang=python3
+
+print("Hello World")
---
+++ Learning/MasterPython_And_DSA/StriverA2ZSheet/Step1_Learn_The_Basics/Lec1/01-User_Input_Output/05.Input_In_Python.py
@@ -0,0 +1,13 @@
+# Prioblem link:- https://www.geeksforgeeks.org/problems/input-in-python/1&selectedLang=python3
+
+# Take string input and print the string input
+st = input()
+print(st)
+# Take integer input and add 10 to the integer input and print
+it = int(input())
+print(it+10)
+# Take floating-point input and multiply the float input by 10 and print
+ft = float(input())
+print(ft*10)
+########### Write your code above ###############
+
