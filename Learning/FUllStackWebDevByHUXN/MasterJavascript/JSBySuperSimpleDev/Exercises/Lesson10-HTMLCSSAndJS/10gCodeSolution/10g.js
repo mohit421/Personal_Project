924bdda76c32a461cf0a3dba6167a5fa7fdf0269
Mohit <mohitjais312@gmail.com>
2025-05-21 00:28:06 +0530

    solved lesson 10 exercise completly

--- Learning/FUllStackWebDevByHUXN/MasterJavascript/JSBySuperSimpleDev/Exercises/Lesson10-HTMLCSSAndJS/10gCodeSolution/10g.js
+++ Learning/FUllStackWebDevByHUXN/MasterJavascript/JSBySuperSimpleDev/Exercises/Lesson10-HTMLCSSAndJS/10gCodeSolution/10g.js
@@ -2,10 +2,20 @@ function toggleButton(selector){
     const gameBtn = document.querySelector(selector);
     const btnGame = gameBtn.classList.contains('is-toggled');
     if(!btnGame){
-        gameBtn.classList.add('is-toggled');
+        // before turning this button ON, check if there's 
+        // already a button that's turned ON and turn it OFF.
+        turnOffPreviousButton();
+
+        gameBtn.classList.add("is-toggled");
     }
     else{
         gameBtn.classList.remove('is-toggled');
     }
 }
        
+function turnOffPreviousButton() {
+    const previousBtn = document.querySelector(".is-toggled")
+    if (previousBtn) {
+        previousBtn.classList.remove('is-toggled')
+    }
+}
