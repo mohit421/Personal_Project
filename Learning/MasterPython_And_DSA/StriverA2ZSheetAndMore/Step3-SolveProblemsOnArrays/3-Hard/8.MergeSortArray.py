a8c2858bb7757ff3a44655d76058dfc5d5be553f
Mohit <mohitjais312@gmail.com>
2025-07-08 11:16:56 +0530

    learned missing and repeated values

---
+++ Learning/MasterPython_And_DSA/StriverA2ZSheetAndMore/Step3-SolveProblemsOnArrays/3-Hard/8.MergeSortArray.py
@@ -0,0 +1,79 @@
+'''
+Problem youtube solution link:- vhttps://www.youtube.com/watch?v=P1Ic85RarKY&ab_channel=NeetCode
+
+
+'''
+
+# Solution 1 Brute force
+'''
+TC:- O(N) + O((M+N)Log(M+N))
+SC:- (1)
+'''
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        for i in range(m,m+n):
+            nums1[i] = nums2[i-m]
+        nums1.sort()
+        return nums1
+    
+
+# Solution 2
+#  USing merge sort copncept 
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        i,j,k = 0,0,0
+        res = [0]*(m+n)
+        while i<m and j<n:
+            if nums1[i] < nums2[j]:
+                res[k] = nums1[i]
+                i += 1
+            else:
+                res[k] = nums2[j]
+                j += 1
+            k += 1
+        
+        while i<m:
+            res[k] = nums1[i]
+            i,k = i+1, k+1
+        while j<n:
+            res[k] = nums2[j]
+            j,k = j+1,k+1
+        
+        for k in range(n+m):
+            nums1[k] = res[k]
+
+# Soluiton 3 optimised solution 
+
+# Wihout using extra spaces aray inplace
+'''
+
+'''
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        last = m+n -1
+
+        while n > 0 and m> 0:
+            if nums1[m-1] > nums2[n-1]:
+                nums1[last] = nums1[m-1]
+                m -= 1
+            else:
+                nums1[last] = nums2[n-1]
+                n -= 1
+            last -= 1
+        
+        while n>0:
+            nums1[last] = nums2[n-1]
+            n, last  = n-1, last-1
+        
+
---
+++ Learning/MasterPython_And_DSA/StriverA2ZSheetAndMore/Step3-SolveProblemsOnArrays/3-Hard/9.FindMissingAndRepeatedvalues.py
@@ -0,0 +1,99 @@
+'''
+link:- https://leetcode.com/problems/find-missing-and-repeated-values/description/
+'''
+
+# Solution 1 brute force 
+'''
+TC:- O(N^2) + O(N^2) = O(2*N^2) = O(N^2)
+SC:- O(N^2) :- bcuz storing freq element till 1 to n^2
+
+'''
+class Solution:
+    def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
+        ans = []
+        a,b = 0,0
+        n = len(grid)
+        mpp = {}
+        for arr in grid:
+            for j in arr:
+                mpp[j] = mpp.get(j,0) + 1
+
+        repeated = missing = 0
+        total = n*n
+        for i in range(1,total+1):
+            count = mpp.get(i,0)
+            if count == 0:
+                missing = i
+            elif count == 2:
+                repeated = i
+        return [repeated,missing]
+    
+# Similar to above but using defaultdict()
+
+class Solution:
+    def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
+        n = len(grid)
+        count = defaultdict(int)
+        for i in range(n):
+            for j in range(n):
+                count[grid[i][j]] += 1
+
+        missing, repeated = 0,0
+
+        for i in range(1,n*n+1):
+            if count[i] == 0:
+                missing = i
+            elif count[i] == 2:
+                repeated = i
+        return [repeated,missing]
+    
+
+# Similar to aboe but uisng simple {}
+
+class Solution:
+    def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
+        n = len(grid)
+        count = {}
+        for i in range(n):
+            for j in range(n):
+                if grid[i][j] not in count:
+                    count[grid[i][j]] = 0
+                count[grid[i][j]] += 1
+
+        missing, repeated = 0,0
+
+        for i in range(1,n*n+1):
+            if i not in count:
+                missing = i
+            elif count[i] == 2:
+                repeated = i
+        return [repeated,missing]
+
+# Solution 2  using mathematical equations
+'''
+TC:- O(n^2)
+SC:- O(1)
+'''
+
+class Solution:
+    def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
+        n = len(grid)
+        total = n*n
+        expected_sum = total * (total + 1) // 2
+        expected_sq_sum = total * (total + 1) * (2 * total + 1) // 6
+
+        actual_sm = actual_sq_sum = 0
+
+        for row in grid:
+            for col in row:
+                actual_sm += col
+                actual_sq_sum += col*col
+        
+        diff = actual_sm - expected_sum #y-x
+        sq_diff = actual_sq_sum - expected_sq_sum   #y^2 - x^2
+        # y^2-x^2 = (y+x)(y-x)
+        sum_yx = sq_diff//diff  # y+x
+        y = (diff+sum_yx)//2
+        x = y- diff
+        return [y,x]
+
